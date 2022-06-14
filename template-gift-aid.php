95f098df533939a2cfe95473bcd396512d49ad16
Rajiv <r_lodhia@hotmail.com>
2022-06-14 09:34:54 +0100

    Render the explanation field on checkout

--- template-gift-aid.php
+++ template-gift-aid.php
@@ -2,9 +2,16 @@
 
 	<?php
 
+    // Get the title setting from WC.
 	$title = WC_Admin_Settings::get_option('gift_aid__title');
 	if ( !empty( $title ) ) {
 		echo '<h2>' . __( $title ) . '</h2>';
+	}
+
+	// Get the explanation setting from WC.
+    $explanation = WC_Admin_Settings::get_option('gift_aid__explanation');
+	if ( !empty( $explanation ) ) {
+		echo '<p>' . __( $explanation ) . '</p>';
 	}
 
 	woocommerce_form_field( 'woocommerce_gift_aid', array(
