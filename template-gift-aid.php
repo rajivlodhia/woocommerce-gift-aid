9c40ae46deff4176ba074552ba8eb7384fc1178b
Rajiv <r_lodhia@hotmail.com>
2022-07-05 10:45:12 +0100

    Add new filters to the gift aid template
    Also includes spacing and wpautop() usage

--- template-gift-aid.php
+++ template-gift-aid.php
@@ -1,20 +1,22 @@
-<div id="woocommerce_gift_aid">
+<div id="woocommerce-gift-aid">
 
 	<?php
 
-    // Get the title setting from WC.
-	$title = WC_Admin_Settings::get_option('gift_aid__title');
+	// Get the title setting from WC.
+	$title = WC_Admin_Settings::get_option( 'gift_aid__title' );
+	$title = apply_filters( 'wcga_render_title_field', $title );
 	if ( !empty( $title ) ) {
-		echo '<h2>' . __( $title ) . '</h2>';
+		echo '<h2 class="woocommerce-gift-aid--title">' . __( $title ) . '</h2>';
 	}
 
 	// Get the explanation setting from WC.
-    $explanation = WC_Admin_Settings::get_option('gift_aid__explanation');
+	$explanation = WC_Admin_Settings::get_option( 'gift_aid__explanation' );
+	$explanation = apply_filters( 'wcga_render_explanation_field', $explanation );
 	if ( !empty( $explanation ) ) {
-		echo '<div>' . wpautop( __( $explanation ) ) . '</div>';
+		echo '<div class="woocommerce-gift-aid--explanation">' . wpautop( __( $explanation ) ) . '</div>';
 	}
 
-	$label = WC_Admin_Settings::get_option('gift_aid__checkbox_text');
+	$label = WC_Admin_Settings::get_option( 'gift_aid__checkbox_text' );
 	woocommerce_form_field( 'woocommerce_gift_aid', array(
 		'type'        => 'checkbox',
 		'class'       => array(
